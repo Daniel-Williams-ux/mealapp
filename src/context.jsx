febf60b00f22be53d900bf3f47a3d43de1a63f8b
Daniel Williams <williamsoligodaniel@gmail.com>
2023-01-30 10:32:12 +0000

    1.create app context with react create context 2. write app provider function that will wrap the app context and return app context

--- src/context.jsx
+++ src/context.jsx
@@ -1,1 +1,12 @@
 import React, {useContext} from 'react';
+
+const AppContext = React.createContext();
+
+
+const AppProvider = ({children}) => {
+  return <AppContext.Provider value="Hello">
+    {children}
+  </AppContext.Provider>
+}
+
+export default(AppContext, AppProvider);
